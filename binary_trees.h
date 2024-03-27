dde5b5cc505b7818d323b2df93236bdf6894af76
VectorGits <davidoluremi0@gmail.com>
2024-03-27 05:55:42 +0100

    Add binary_tree_node function implementation

--- binary_trees.h
+++ binary_trees.h
@@ -6,6 +6,42 @@
 #include <string.h>
 #include <stddef.h>
 
+/* Data Structures */
+
+/**
+ *struct binary tree s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/**
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
+/* Printing helper function */
+void binary_tree_print(const binary_tree_t *);
 
 
 
