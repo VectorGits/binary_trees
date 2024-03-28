4d915f772d731d8d40da21a7ba73ff853d577775
VectorGits <davidoluremi0@gmail.com>
2024-03-28 16:11:43 +0100

    Implement new functions for binary trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -17,7 +17,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	if (tree->left != NULL && tree->right != NULL)
+	{
 		left_full = binary_tree_is_full(tree->left);
 		right_full = binary_tree_is_full(tree->right);
 		return (left_full && right_full);
+	}
+	return (0);
 }
