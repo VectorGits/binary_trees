7a90a2cc14dc884efe38e3119b868ded568997c2
VectorGits <davidoluremi0@gmail.com>
2024-04-01 18:20:54 +0100

    Add binary_tree_levelorder function

--- binary_trees.h
+++ binary_trees.h
@@ -71,4 +71,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* ADVANCED TASKS */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 #endif /* Binary Trees */
